513ba7a9ac6091f5f215f212cfde0430e2500cc4
Rowan <rowanb99@byu.edu>
2022-10-31 21:35:59 -0600

    can now actually start because wasn't able to debug

---
+++ cmake-build-debug-schizo/CMakeFiles/project2.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/r/rowanb99/project2/AddAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/AddAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/BlockComment_Automaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/BlockComment_Automaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/ColonAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/ColonAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/ColonDashAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/ColonDashAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/CommAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/CommAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/EOF_Automaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/EOF_Automaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/FactsAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/FactsAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/ID_Automaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/ID_Automaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/Left_ParenAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/Left_ParenAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/Lexer.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/Lexer.cpp.o"
+  "/users/guest/r/rowanb99/project2/LineCommentAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/LineCommentAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/MultiplyAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/MultiplyAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/PeriodAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/PeriodAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/Q_markAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/Q_markAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/QueriesAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/QueriesAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/Right_ParenAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/Right_ParenAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/RulesAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/RulesAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/SchemesAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/SchemesAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/StringAutomaton.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/StringAutomaton.cpp.o"
+  "/users/guest/r/rowanb99/project2/Token.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/Token.cpp.o"
+  "/users/guest/r/rowanb99/project2/main.cpp" "/users/guest/r/rowanb99/project2/cmake-build-debug-schizo/CMakeFiles/project2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-schizo/CMakeFiles/project2.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project2.dir/AddAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/BlockComment_Automaton.cpp.o"
+  "CMakeFiles/project2.dir/ColonAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/ColonDashAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/CommAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/EOF_Automaton.cpp.o"
+  "CMakeFiles/project2.dir/FactsAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/ID_Automaton.cpp.o"
+  "CMakeFiles/project2.dir/Left_ParenAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/Lexer.cpp.o"
+  "CMakeFiles/project2.dir/LineCommentAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/MultiplyAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/PeriodAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/Q_markAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/QueriesAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/Right_ParenAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/RulesAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/SchemesAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/StringAutomaton.cpp.o"
+  "CMakeFiles/project2.dir/Token.cpp.o"
+  "CMakeFiles/project2.dir/main.cpp.o"
+  "project2"
+  "project2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
